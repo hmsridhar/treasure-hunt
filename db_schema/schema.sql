1f1b7675f080eb87d15b8fd583dc51c9beb100c8
harshams <hmsridhar@gigsky.com>
2020-04-22 00:44:03 +0530

    added team_id column to user table

--- db_schema/schema.sql
+++ db_schema/schema.sql
@@ -124,13 +124,15 @@ DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `username` varchar(100) NOT NULL,
-  `password` varchar(100) NOT NULL,
+  `password` varchar(200) NOT NULL,
   `role` varchar(50) NOT NULL,
-  `team` varchar(100) DEFAULT NULL,
-  `token` varchar(255) DEFAULT NULL,
-  `token_expiry_time` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+  `team_id` bigint(20) NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `token_expiry_time` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `team_id` (`team_id`),
+  CONSTRAINT `user_ibfk_1` FOREIGN KEY (`team_id`) REFERENCES `team` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -142,4 +144,4 @@ CREATE TABLE `user` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-04-21 15:35:56
+-- Dump completed on 2020-04-22  0:43:21
